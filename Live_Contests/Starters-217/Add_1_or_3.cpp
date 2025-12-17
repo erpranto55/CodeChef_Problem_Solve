0b9785f93de6169146ff459bc11c50be6592cc4b
ER Pranto <erpranto55@gmail.com>
2025-12-17 23:57:47 +0600

    Starters-216

---
+++ Live_Contests/Starters-217/Add_1_or_3.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define FAST ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define HA cout << "YES" << '\n';
+#define NA cout << "NO" << '\n';
+#define nl '\n'
+#define mod 1000000007
+#define N 500005
+ll fx[] = {0, 0, 1, -1, 1, 1, -1, -1};
+ll fy[] = {1, -1, 0, 0, -1, 1, -1, 1};
+
+int main()
+{
+    FAST;
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        ll n, m;
+        cin >> n >> m;
+        if (m >= n && m <= 3 * n && (m - n) % 2 == 0)
+            HA else NA
+    }
+    return 0;
+}
